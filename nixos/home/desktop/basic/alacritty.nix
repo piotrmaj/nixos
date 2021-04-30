9db8c806fe10219587f382bd7a4377e3e133ca01
Piotr Maj piotrmaj17@gmail.com <piotrmaj17@gmail.com>
2021-04-30 17:29:07 +0200

    update

--- nixos/home/desktop/basic/alacritty.nix
+++ nixos/home/desktop/basic/alacritty.nix
@@ -24,8 +24,8 @@ in {
       - { key: Copy,     action: Copy  }
       - { key: C,        action: Copy,             mods: Super }
       - { key: V,        action: Paste,            mods: Super }
-      - { key: Equals,   action: IncreaseFontSize, mods: Super }
-      - { key: Subtract, action: DecreaseFontSize, mods: Super }
+      - { key: Plus,   action: IncreaseFontSize, mods: Super }
+      - { key: NumpadSubtract, action: DecreaseFontSize, mods: Super }
 
     font:
       bold:

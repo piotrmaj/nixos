abf95f8badcf4e4dcdbf99b0c497d708a73484a2
Piotr Maj <piotrmaj17@gmail.com>
2021-05-01 20:12:47 +0200

    fix

--- nixos/home.nix
+++ nixos/home.nix
@@ -3,27 +3,17 @@
   # Let Home Manager install and manage itself
   programs.home-manger-enable = true;
 
-  nixpkgs = {
-        overlays = [
-        (import ./pkgs/default.nix)
-        ];
-        config.allowUnfree = true;
-
-        # Allow certain unfree programs to be installed.
-        config = {
-          allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-              # "discord"
-              # "faac"
-              # "postman"
-              # "slack"
-              "spotify"
-              # "steam"
-              # "steam-original"
-              # "steam-runtime"
-              # "zoom-us"
-          ];
-        };
-    };
+  home.packages = with pkgs; [
+    # "discord"
+    # "faac"
+    # "postman"
+    # "slack"
+    "spotify"
+    # "steam"
+    # "steam-original"
+    # "steam-runtime"
+    # "zoom-us"
+  ]
 
   imports = [
     ./home/terminal/basic.nix

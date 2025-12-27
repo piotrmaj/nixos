1fc38ab2cf6d612f4706108b2d75ef13815440b2
Piotr Maj <piotrmaj17@gmail.com>
2025-12-27 23:19:36 +0100

    fix

--- new/modules/home/sops-nix.nix
+++ new/modules/home/sops-nix.nix
@@ -1,9 +1,9 @@
-{ settings, host, ... }:
+{ ... }:
 {
   inputs.sops-nix.url = "github:Mic92/sops-nix";
   inputs.sops-nix.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = { self, nixpkgs, sops-nix }: {
+  outputs = { self, nixpkgs, host, sops-nix }: {
     nixosConfigurations.${host} = nixpkgs.lib.nixosSystem {
       modules = [
         sops-nix.nixosModules.sops

39387caff94d639caec46f485b3bb7d693b3af00
Piotr Maj <piotrmaj17@gmail.com>
2025-12-27 21:36:14 +0100

    cleanup

--- new/hosts/vm/hardware-configuration.nix
+++ new/hosts/vm/hardware-configuration.nix
@@ -6,19 +6,9 @@
 {
   imports = [ ];
 
-  boot.initrd.availableKernelModules = [ 
-    "ata_piix" 
-    "ohci_pci"
-    "ehci_pci"
-    "ahci"
-    "sd_mod"
-    "sr_mod"
-    "vboxguest"
-    "vboxvideo"
-    "vwgfx" 
-  ];
+  boot.initrd.availableKernelModules = [ "ata_piix" "ohci_pci" "ehci_pci" "ahci" "sd_mod" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" "vboxguest" "vboxvideo" ];
+  boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
